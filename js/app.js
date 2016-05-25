f582396c4089549413b4795a00e57c84b7bdcd11
Tim Creasy <timcreasy@me.com>
2016-05-25 17:30:52 -0500

    Added tree function, passing in tree object per requirements

--- js/app.js
+++ js/app.js
@@ -10,23 +10,17 @@ var button = document.getElementById("inputButton");
 var numberOfChars = 1;
 var charString = "";
 
-// Pine tree object
-var pineTree = {
-  height: null,
-  charToUse: null
-};
-
 
 // ================================= FUNCTIONS =================================
 
 // Function to build tree
-function buildTree() {
+function tree(treeToBuild) {
   numberOfChars = 1;  
-  var numberOfSpaces = pineTree.height - 1;
-  for(var i = 0; i < pineTree.height; i++) {
+  var numberOfSpaces = treeToBuild.height - 1;
+  for(var i = 0; i < treeToBuild.height; i++) {
     addBlanks(numberOfSpaces);
-    buildCharString();
-    printCharString();
+    addChars(treeToBuild.charToUse);
+    printLine();
     numberOfSpaces--;
   }
   charString = "";
@@ -37,9 +31,12 @@ function getInput() {
   if(charInput.value === "" || heightInput.value === "") {
     alert("Please enter both values to continue");
   } else {
-    pineTree.height = heightInput.value;
-    pineTree.charToUse = charInput.value;
-    buildTree();
+    // Pine tree object
+    var pineTree = {
+      height: heightInput.value,
+      charToUse: charInput.value
+    };
+    tree(pineTree);
   }
 }
 
@@ -51,15 +48,15 @@ function addBlanks(x) {
 }
 
 // Add characters to properly offset string
-function buildCharString() {
+function addChars(theChar) {
   for (var i = 1; i <= numberOfChars; i++) {
-    charString += pineTree.charToUse;
+    charString += theChar;
   }
   numberOfChars += 2;
 }
 
 // Print built string
-function printCharString() {
+function printLine() {
   console.log(charString + "\n");
   charString = "";
 }
